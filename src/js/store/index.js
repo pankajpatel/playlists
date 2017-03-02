dd40ecf3c4e35f674f75f14536202ccb3cca264b
Pankaj Patel <patel.pankaj@live.in>
2017-03-02 08:57:31 +0100

    added middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import api from '../middleware/api'
+import api from '../middleware/api'
 import rootReducer from '../reducers'
 
 const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
-  // applyMiddleware(thunk, api)
+  applyMiddleware(thunk, api)
 )
 
 export default configureStore
